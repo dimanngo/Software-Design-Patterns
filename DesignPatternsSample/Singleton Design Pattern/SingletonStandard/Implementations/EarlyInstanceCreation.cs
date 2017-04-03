96ae95a9938c92d3145539583bb34030f1ec7f2b
Dmytro Golodiuk <Dmytro.Golodiuk@zeppelin.com>
2017-04-03 14:33:41 +0000

    Add new Singleton implementation: Early instance creation.

---
+++ DesignPatternsSample/Singleton Design Pattern/SingletonStandard/Implementations/EarlyInstanceCreation.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Early instance creation.
+ * We can choose to create the instance of Singleton class when the class is loaded.
+ * This is thread-safe without using locking.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton.Implementations
+{
+    public class EarlyInstanceCreation : ISingleton
+    {
+        private static int countInstances = 0;
+        private static EarlyInstanceCreation instance = new EarlyInstanceCreation();
+
+        int ISingleton.CountInstances => countInstances;
+
+        private EarlyInstanceCreation()
+        {
+            Interlocked.Increment(ref countInstances);
+        }
+
+        ~EarlyInstanceCreation()
+        {
+            Interlocked.Decrement(ref countInstances);
+        }
+
+        public static ISingleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
--- DesignPatternsSample/Singleton Design Pattern/SingletonStandard/Program.cs
+++ DesignPatternsSample/Singleton Design Pattern/SingletonStandard/Program.cs
@@ -46,7 +46,8 @@ namespace Singleton
             {
                 { 1, typeof(Implementations.Standard).ToString()},
                 { 2, typeof(Implementations.HandleMultithreading).ToString() },
-                { 3, typeof(Implementations.HandleMultithreadingDblCheck).ToString() }
+                { 3, typeof(Implementations.HandleMultithreadingDblCheck).ToString() },
+                { 4, typeof(Implementations.EarlyInstanceCreation).ToString() }
             };
 
             Console.WriteLine("Accessible Singleton Pattern implementations:");
