867a83ab3256e98edf90b3b48abb29711b1bd7a0
dmytro.golodiuk <dmytro.golodiuk@zeppelin.com>
2017-04-03 11:11:30 +0300

    Migration from TFVC

---
+++ StructuralPatterns_RealWorldExamples/AdapterPattern/Adapters/JSONSerializerAdapter.cs
@@ -0,0 +1,14 @@
+﻿using AdapterPattern.Target;
+using System.Web.Script.Serialization;
+
+namespace AdapterPattern.Adapters
+{
+    public class JSONSerializerAdapter:ISerializerAdapter
+    {
+        public string Serialize<T>(object objToSerialize)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(objToSerialize);
+        }
+    }
+}
