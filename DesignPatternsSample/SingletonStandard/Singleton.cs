b416824296de147da86a5d3c865791e95a815c16
Dmytro Golodiuk <Dmytro.Golodiuk@zeppelin.com>
2017-04-03 10:17:56 +0000

    Add singleton pattern implementation: standard method

---
+++ DesignPatternsSample/SingletonStandard/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
